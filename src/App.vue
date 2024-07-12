eb2e6034146c51b43ba4d2b03611f036bf63c109
datduong2k2 <94790149+datduong2k2@users.noreply.github.com>
2024-07-12 09:37:56 +0700

    Revert "Dat homepage"

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,15 @@
 <template>
-  <div>
-    <header-page />
-    <slide-show />
-    <search-bar-mobile />
-    <navbar-mobile />
-    <img
-      src="https://truyen.tangthuvien.vn/images/banner-app-ttv.jpg"
-      alt="tải app"
-      class="full-width-image"
-    />
-    <new-update />
-    <top-view />
-    <recommant-book />
-    <completed-book />
-    <footer-page></footer-page>
-    <!--<navba-page />
-   
-    <top-view />
-    <footer-page /-->
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HeaderPage from "./components/HeaderPage.vue";
-import SearchBarMobile from "./components/SearchBarMobile.vue";
-import NavbarMobile from "./components/NavbarMobile.vue";
-// import NavbaPage from "./components/NavbaPage.vue";
-import SlideShow from "./components/SlideShow.vue";
-import NewUpdate from "./components/NewUpdate.vue";
-import FooterPage from "./components/FooterPage.vue";
-import TopView from "./components/TopView.vue";
-import RecommantBook from "./components/RecommentBook.vue";
-import CompletedBook from "./components/CompletedBook.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
-    HeaderPage,
-    //NavbaPage,
-    SlideShow,
-    SearchBarMobile,
-    NavbarMobile,
-    NewUpdate,
-    FooterPage,
-    TopView,
-    RecommantBook,
-    CompletedBook,
+    HelloWorld,
   },
 };
 </script>
@@ -56,10 +20,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #585c5f;
-}
-.full-width-image {
-  width: 100%;
-  height: auto; /* Đảm bảo tỷ lệ hình ảnh không bị bóp méo */
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
